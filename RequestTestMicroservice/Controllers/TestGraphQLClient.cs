b0978514c607777ac5f969098837378f94193dfc
Máté <34075771+leopoldus97@users.noreply.github.com>
2021-05-30 12:58:29 +0200

    Add RequestTestMicroservice

---
+++ RequestTestMicroservice/Controllers/TestGraphQLClient.cs
@@ -0,0 +1,39 @@
+﻿using GraphQL;
+using GraphQL.Client.Abstractions;
+using GraphQL.Client.Http;
+using RequestTestMicroservice.Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RequestTestMicroservice.Controllers
+{
+    public class TestGraphQLClient
+    {
+        private readonly GraphQLHttpClient _client;
+        public TestGraphQLClient(GraphQLHttpClient client)
+        {
+            _client = client;
+        }
+
+        public async Task<List<Movie>> GetMoviesAsync()
+        {
+            var query = new GraphQLRequest
+            {
+                Query = @"
+                        query movies {
+                            movies {
+                                id
+                                title
+                                duration
+                        "
+            };
+            var response = await _client.SendQueryAsync<List<Movie>>(query);
+            if (response.Errors != null && response.Errors.Any())
+                throw new ApplicationException(response.Errors[0].Message);
+            var movies = response.Data;
+            return movies;
+        }
+    }
+}
---
+++ RequestTestMicroservice/Core/Entity/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RequestTestMicroservice.Core.Entity
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Duration { get; set; }
+    }
+}
